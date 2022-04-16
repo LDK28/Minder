e71bf853e7c03b29d3c0f6f0bd3fc08b340181a9
p1xel <dima.neumoin1234@gmail.com>
2022-04-16 08:31:03 +0300

    add more tests

--- server/Database/DatabaseSessionClient/DatabaseSessionClient.cpp
+++ server/Database/DatabaseSessionClient/DatabaseSessionClient.cpp
@@ -1,10 +1,11 @@
 #include "DatabaseSessionClient.h"
 
-json DatabaseSessionClient::createSession(json) { return json{{}}; }
+json DatabaseSessionClient::createSession() { return json{{}}; }
 json DatabaseSessionClient::addUsersInSession(json) { return json{{}}; }
 bool DatabaseSessionClient::checkSession(int id) { return true; }
+json DatabaseSessionClient::updateSession(json) { return json{{}}; }
 json DatabaseSessionClient::getSessionInfo(int id) { return json{{}}; }
-std::vector<int> DatabaseSessionClient::selectSessionsWithUser(int userId) {
+json DatabaseSessionClient::selectSessionsWithUser(int userId) {
     return json{{}};
 }
 json DatabaseSessionClient::deleteSession(int id) { return json{{}}; }
