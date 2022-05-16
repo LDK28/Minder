e542c36393163bf769eadc4e7b3a0aba178dc3f2
DK <dkostev68@gmail.com>
2022-05-16 11:31:04 +0300

    Fix tabular

--- ClientConnection/HttpClient/HttpClientData.hpp
+++ ClientConnection/HttpClient/HttpClientData.hpp
@@ -3,54 +3,54 @@
 struct HttpClientData
 {
 public:
-   struct Font
-   {
-       std::string family;
-       int pointSize;
-       int weight;
-       bool italic;
-   };
+    struct Font
+    {
+        std::string family;
+        int pointSize;
+        int weight;
+        bool italic;
+    };
 
-   struct Color
-   {
-       int r;
-       int g;
-       int b;
-   };
+    struct Color
+    {
+        int r;
+        int g;
+        int b;
+    };
 
-   struct Block
-   {
-       size_t id;
-       size_t parentId;
-       int posX;
-       int posY;
-       std::string text;
-       Font font;
-       Color fontColor;
-       Color borderColor;
-       Color bgColor;
-   };
+    struct Block
+    {
+        size_t id;
+        size_t parentId;
+        int posX;
+        int posY;
+        std::string text;
+        Font font;
+        Color fontColor;
+        Color borderColor;
+        Color bgColor;
+    };
 
-   enum returnCode {
-       SUCCESS,
-       FAILED
-   };
-   
-   struct SettingsData
-   {
-       std::string ip;
-       std::string port;
-   };
+    enum returnCode
+    {
+        SUCCESS,
+        FAILED
+    };
 
-   struct SessionConnectionData
-   {
-       size_t id;
-       std::string password;
-   };
-   
-   struct MidnMapData
-   {
-       std::vector<Block> blocks;
-   };
-   
+    struct SettingsData
+    {
+        std::string ip;
+        std::string port;
+    };
+
+    struct SessionConnectionData
+    {
+        size_t id;
+        std::string password;
+    };
+
+    struct MidnMapData
+    {
+        std::vector<Block> blocks;
+    };
 };
