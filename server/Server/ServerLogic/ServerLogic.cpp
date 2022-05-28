d795b7af90b1465720448e67fd6cc5ea79f83148
LDK28 <dkostev68@gmail.com>
2022-05-28 09:44:30 +0300

    Simple server

--- server/Server/ServerLogic/ServerLogic.cpp
+++ server/Server/ServerLogic/ServerLogic.cpp
@@ -1,26 +1,86 @@
 #include "ServerLogic.hpp"
 
-int ServerLogic::getConfID() {}
-void ServerLogic::setConfID(int confID) {}
-json ServerLogic::getDataFromDB(char &request) {}
-void ServerLogic::sendDataToDB(json) {}
-
 std::string ServerLogic::router(std::string &request)
 {
-    std::string response;
+    std::string response = "";
     json data = json::parse(request);
-    if (data["title"] == "GETUSERS")
-        response = this->ActiveUsers.dump();
+
     if (data["title"] == "CHECKCONNECTIONTOSESSION")
     {
+        int id = data["session"]["id"];
+        std::string password = data["session"]["password"];
+        json dbResponse = DBSessionClient.checkConnectionToSession(id, password);
+        if (dbResponse["status"] == "ok")
+        {
+            std::string id = dbResponse["sessions"]["id"];
+            int sessionId = std::stoi(id, nullptr, 10);
+            std::vector<int> userId;
+            userId.push_back(data["userId"]);
+            DBUsersClient.addUsersInSession(userId, sessionId);
+
+            response = dbResponse["sessions"]["name"];
+        }
     }
     if (data["title"] == "CREATESESSION")
     {
+        DBSessionClient.createSession(data["session"]);
+        json session = DBSessionClient.getSessionInfo(data["session"]);
+        std::vector<int> userId;
+        userId.push_back(data["userId"]);
+        std::string sessionId = session["id"];
+        json dbresponse = DBUsersClient.addUsersInSession(userId, std::stoi(sessionId));
+        response = dbresponse["id"];
     }
+    if (data["title"] == "ADDBLOCK")
+    {
+        int deskId = data["deskId"];
+        json block = data["block"];
+        block["desk_id"] = deskId;
+        block.erase("id");
+        json dbResponse = DBDrawDeskClient.addBlock(deskId, block);
+        response = dbResponse["id"];
+    }
+    if (data["title"] == "CHANGEBLOCK")
+    {
+        DBDrawDeskClient.updateBlock(data["block"]);
+    }
+
     if (data["title"] == "DELETEBLOCK")
     {
+        int blockId = data["blockId"];
+        DBDrawDeskClient.deleteBlock(blockId);
     }
     if (data["title"] == "GETCURRENTSTATEDESK")
     {
+        int deskId = data["sessionId"];
+        json blocks = DBDrawDeskClient.getBlocksInfoFromDesk(deskId);
+        response = blocks.dump();
     }
+    if (data["title"] == "GETUSERS")
+    {
+        int sessionId = data["sessionId"];
+        json dbResponse = DBUsersClient.getUsersInSession(sessionId);
+        response = dbResponse.dump();
+
+    }
+    if (data["title"] == "LOGINUSER")
+    {
+        std::string name = data["user"]["nickname"];
+        std::string password = data["user"]["password"];
+
+        bool check = DBUsersClient.validateUser(name, password);
+        if (check == true)
+        {
+            json user = DBUsersClient.getUserByName(name);
+            int id = user["users"][0]["id"];
+            response = std::to_string(id);
+        }
+    }
+    if (data["title"] == "REGISTERUSER")
+    {
+        json dbRespose = DBUsersClient.createUser(data["user"]);
+        response = dbRespose["id"];
+    }
+
+    return response;
 }
---
+++ server/build/CMakeFiles/MinderServer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ldk/Рабочий стол/Minder/server/main.cpp" "/home/ldk/Рабочий стол/Minder/server/build/CMakeFiles/MinderServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././Server/NetworkController"
+  "../Server/NetworkController/../../nlohmann"
+  "../Server/NetworkController/../."
+  "../Server/ServerLogic/../../nlohmann"
+  "../Server/ServerLogic/../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ldk/Рабочий стол/Minder/server/build/Server/NetworkController/CMakeFiles/NetworkController.dir/DependInfo.cmake"
+  "/home/ldk/Рабочий стол/Minder/server/build/Server/ServerLogic/CMakeFiles/ServerLogic.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
